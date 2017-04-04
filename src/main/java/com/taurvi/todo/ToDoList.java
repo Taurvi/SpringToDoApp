a20544f09f615e7d36804282bddbc0d2d6c2317a
Stephen Rimbakusumo <srimbauksumo@gmail.com>
2017-04-03 22:08:56 -0700

    Completed ToDoList class and tests

--- src/main/java/com/taurvi/todo/ToDoList.java
+++ src/main/java/com/taurvi/todo/ToDoList.java
@@ -1,7 +1,24 @@
 package com.taurvi.todo;
 
+import com.sun.xml.internal.bind.v2.TODO;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class ToDoList {
+    private List<ToDoItem> list;
 
     public ToDoList() {
+        this.list = new ArrayList<ToDoItem>();
+    }
+
+    public void add(ToDoItem toDoItem) {
+        list.add(toDoItem);
+    }
+
+    public void markDone(int index) {
+        list.get(index).toggleDone();
     }
 }
--- src/test/java/com/taurvi/todo/ToDoListTests.java
+++ src/test/java/com/taurvi/todo/ToDoListTests.java
@@ -1,12 +1,23 @@
 package com.taurvi.todo;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.ThrowableAssert.catchThrowable;
 
 public class ToDoListTests {
+    private ToDoList toDoList;
+    private ToDoItem item;
+
+    @Before
+    public void setup() {
+        toDoList = new ToDoList();
+        item = new ToDoItem("Test");
+        toDoList.add(item);
+    }
+
     @Test
     public void should_exist() {
         // Given
@@ -14,5 +25,22 @@ public class ToDoListTests {
         Throwable error = catchThrowable(() -> Class.forName("com.taurvi.todo.ToDoList"));
         // Then
         assertThat(error).isNull();
+    }
+
+    @Test
+    public void should_add_items_to_list() {
+        // Given
+        // When
+        // Then
+        assertThat(toDoList.getList().get(0)).isEqualTo(item);
+    }
+
+    @Test
+    public void should_mark_items_as_done() {
+        // Given
+        // When
+        toDoList.markDone(0);
+        // Then
+        assertThat(toDoList.getList().get(0).isDone()).isTrue();
     }
 }
